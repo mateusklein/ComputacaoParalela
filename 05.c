0b8649dee256c3a067a48bd1757ba00f6b82d978
mkl <mateusklein@terra.com.br>
2023-03-06 15:38:25 -0300

    ex5 correto

--- 05.c
+++ 05.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("HELLO WORD");
-    return 0;
+    int linhas;
+    int colunas;
+    int valor;
+    printf("Numero de linhas:\n");
+    scanf("%d", &linhas);
+    printf("Numero de colunas:\n");
+    scanf("%d", &colunas);
+    int matriz[linhas][colunas];
+    for(int i=1; i<=linhas;i++){
+        for(int j=1; j<=colunas; j++){
+            printf("DIGITE VALOR ELEMENTO %d x %d: \n", i, j);
+            scanf("%d", &valor);
+            matriz[i][j] = valor;
+        }
+    }
+    printf("DIGITE VALOR PARA MULTIPLICAR: \n");
+    scanf("%d", &valor);
+    for(int i=1; i<=linhas;i++){
+        for(int j=1; j<=colunas; j++){
+            matriz[i][j] = matriz[i][j] * valor;
+            printf("ELEMENTO %d x %d: %d", i, j, matriz[i][j]);
+        }
+    }
 }
