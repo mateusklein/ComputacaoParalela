4e79c9349f0ceb53fd47a1d02a5cc616050ee3a2
Ubuntu <ubuntu@ip-172-31-11-55.ec2.internal>
2023-03-22 20:21:26 +0000

    corrigindo lab03

--- lab03.c
+++ lab03.c
@@ -20,8 +20,26 @@ void main(){
   if(id==0){
     printf("Entrou no processo filho \n");
     //AQUI REALIZA A PRIMEIRA OPERAÇÃO
-
-
+   int esc;
+   int n;
+   printf("Digite um numero de 1 a 4 (1-soma, 2-subt, 3-div, 4-mult)\n");
+   scanf("%d", &esc);
+   printf("Digite um numero: \n");
+   scanf("%d", &n);
+   switch(esc){
+	   case 1:
+		   mem[0] = mem[0] + n;
+		   break;
+	   case 2:
+		   mem[0] = mem[0] - n;
+		   break;
+	   case 3:
+		   mem[0] = mem[0] / n;
+		   break;
+	   case 4:
+		   mem[0] = mem[0] * n;
+		   break;
+	}
     printf("Valor da variavel: %d\n", mem[0]);
   }else{
     printf("Entrou no processo pai \n");
@@ -30,7 +48,27 @@ void main(){
     printf("Entrou no processo pai novamente\n");
     //AQUI VAI A SEGUNDA OPERAÇÃO
     
-
+    int esc;
+   int n;
+   printf("Digite um numero de 1 a 4 (1-soma, 2-subt, 3-div, 4-mult)\n");
+   scanf("%d", &esc);
+   printf("Digite um numero: \n");
+   scanf("%d", &n);
+   switch(esc){
+           case 1:
+                   mem[0] = mem[0] + n;
+		   break;
+           case 2:
+                   mem[0] = mem[0] - n;
+		   break;
+           case 3:
+                   mem[0] = mem[0] / n;
+		   break;
+           case 4:
+                   mem[0] = mem[0] * n;
+		   break;
+        }
+	
     printf("Valor da variavel: %d \n", mem[0]);
   }
 }
