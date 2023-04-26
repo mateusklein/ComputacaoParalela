1074627e7ddbfc8b29bb3119d5018ee4f8794327
Mateus Klein <83736407+mateusklein@users.noreply.github.com>
2023-04-26 20:54:50 -0300

    Update mult_matrizes.c

--- lab06parte2/mult_matrizes.c
+++ lab06parte2/mult_matrizes.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-
-pthread_mutex_t mutex;
 
 #define NUM_THREADS 25
 
@@ -11,28 +7,24 @@ int mat[NUM_THREADS][NUM_THREADS];
 int vec[NUM_THREADS];
 int result[NUM_THREADS] = {0};
 
-void *Pth_mat_vect(void *rank);
+void Pth_mat_vect(int rank);
 
 int main(){
-  pthread_t threads[NUM_THREADS];
-  int args[NUM_THREADS];
+  int thread_id;
 
   int cont = 1;
   for(int i=0; i<NUM_THREADS; i++){
-    args[i] = i;
     vec[i] = i+1;
     for(int j=0; j<NUM_THREADS; j++){
       mat[i][j] = cont;
       cont++;
     }
-  } 
-  
-  for (int thread_id = 0; thread_id < NUM_THREADS; thread_id++) {
-    pthread_create(&threads[thread_id], NULL, Pth_mat_vect, &args[thread_id]);
   }
 
-  for (int thread_id = 0; thread_id < NUM_THREADS; thread_id++) {
-    pthread_join(threads[thread_id], NULL);
+  #pragma omp parallel num_threads(NUM_THREADS) private(thread_id)
+  {
+    thread_id = omp_get_thread_num();
+    Pth_mat_vect(thread_id);
   }
 
   for (int i = 0; i < NUM_THREADS; i++) {
@@ -43,12 +35,10 @@ int main(){
   return 0;
 }
 
-void *Pth_mat_vect(void *rank) {
-  pthread_mutex_lock(&mutex);
-  int *i = rank;
+void Pth_mat_vect(int rank) {
+  int i = rank;
   for (int j = 0; j < NUM_THREADS; j++) {
-    result[*i] += mat[*i][j] * vec[j];
+    #pragma omp atomic
+    result[i] += mat[i][j] * vec[j];
   }
-  pthread_mutex_unlock(&mutex);
-  return NULL;
 }
