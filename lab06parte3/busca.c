9c15eecd7af94f87cd9575d4af2a55ad504e525f
Ubuntu <ubuntu@ip-172-31-9-134.ec2.internal>
2023-05-03 19:12:10 +0000

    lab06parte3

--- lab06parte3/busca.c
+++ lab06parte3/busca.c
@@ -1,40 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-# include <omp.h>
+#include <omp.h>
 #include <time.h>
 
-#define NUM_THREADS 10;
+#define NUM_THREADS 10
 
-int main(void){
-  int num = 100;
+int main(){
+  srand(time(NULL));
+  int num = 10;
   int i=0;
   int lista[num]; 
-  int n_proc = rand()%100;   
+  int n_proc = rand()%num;  
+  printf("num procurado: %d \n", n_proc); 
   int index_proc = -1;
-  srand(time(NULL));
   do{
-    lista[i] = rand()%100;
+    lista[i] = rand()%num;
     int igual = 0; 
     for(int j=0; j<i; j++){ 
       if(lista[j] == lista[i]){
         igual = 1;
       }
     }
-    if(igual == 0){ 
+    if(igual == 0){
+      printf("vetor %d: %d \n", i, lista[i]); 
       i++;
     }
   }while(i<num);
     
-  #pragma omp paralel num_threads(NUM_THREADS){
-      int fim = num *  omp_get_thread_num();
-        int ini = fim - (num / omp_get_num_threads());
-        for(int i=ini; i<fim; i++){
-                #pragma omp critical{
-                        if(lista[i]==n_proc){
-                                index_proc = i;
-                        }
-                }
-        }
+  #pragma omp parallel num_threads(NUM_THREADS)
+  {
+      printf("entrou na thread %d \n", omp_get_thread_num());
+      int t = omp_get_thread_num();
+      int num_t = omp_get_num_threads();
+      int intervalo = num/num_t;
+      int ini = t * intervalo;
+      int fim = ini + intervalo;
+      for(int j=ini; j<fim; j++){
+      	 if(lista[j]==n_proc){
+		 #pragma omp critical
+		 {
+			 if(index_proc == -1){
+           		    index_proc = j;
+			 }
+		 }
+           }
+       }
   }
-  printf("Index do elemento procurado: %d", index_proc);
+  printf("Index do elemento procurado: %d \n", index_proc);
 }
